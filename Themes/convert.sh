5b1c05dfb7e15d3822531787d2333d2fddaf24ec
Ruediger Haertel <ruediger.haertel@sensotech.com>
2017-09-10 15:10:59 +0200

    Added Icons Icons are organized in themes Themes could be broken down in pixel sizes

---
+++ Themes/convert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage convert.sh <Theme-Folder>"
+    exit 1
+fi
+
+THEME=$1
+PNG2C=../png2c/png2c
+THEME_HDR=$THEME/$THEME.h
+
+echo "/* Generated on '`date`' */" > $THEME_HDR
+echo -e "\n#pragma once\n" >> $THEME_HDR
+for i in `ls $THEME/*.png`; do
+    HFILE=`dirname $i`/`basename $i .png`.h
+    echo $HFILE
+    $PNG2C $i $HFILE
+
+    echo "#include <Themes/$HFILE>" >> $THEME_HDR
+done
+echo "" >> $THEME_HDR
+
+echo "Generated $THEME_HDR"
+
